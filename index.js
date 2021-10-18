9d2dd33144bcad7210a4f7d5cae6ae6fe206e7dc
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-10-18 13:17:57 +0300

    time from a to b function added

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import moment from 'moment';
 moment().format();
 
 function timeToNow(dateArr){
-    console.log(moment())
-    console.log(moment(dateArr).toNow());
+    return moment(dateArr).toNow();
 }
-timeToNow([2019,10,18]);
+// timeToNow([2019,10,18]);
+
+function timeFromAToB(date1,date2){
+    const momenent1 = moment(date1);
+    const moment2 = moment(date2);
+    return momenent1.from(moment2);
+}
